72bdac56fdae35cc36b94185caa7c96c160832e9
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-12 20:35:33 -0300

    importando primeiro componete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import NotificationButton from "./components/NotificationButton"
 function App() {
   return(
+    <> { /*  dentro de um component react so podemos ter uma teg, entao para adicionarmos mais uma podemos colocar esse <> </> para inserirmos outras  */}
     <h1>Hello world</h1>
+    <NotificationButton />
+    </>
   )
 }
 
