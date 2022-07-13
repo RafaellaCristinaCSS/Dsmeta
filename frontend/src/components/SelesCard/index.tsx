368aabf5c641a24dd8950a9b21b5a1d57f7c83ad
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-12 23:06:53 -0300

    criando o component de card e linkando com o component de button

---
+++ frontend/src/components/SelesCard/index.tsx
@@ -0,0 +1,76 @@
+import NotificationButton from "../NotificationButton"
+import './style.css'
+function SalesCars() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="demeta-saler-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <input type="text" className="dsmeta-form-control" /></div>
+                <div className="dsmeta-form-control-container">
+                    <input type="text" className="dsmeta-form-control" /></div>
+            </div>
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Datas</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">12/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$ 55300,00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">12/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$ 55300,00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">12/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$ 55300,00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+export default SalesCars
