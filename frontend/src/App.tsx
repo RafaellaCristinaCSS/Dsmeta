77dfa26dfb2e06df7bee573578a0ab3eba19150f
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-12 21:36:48 -0300

    Insindo o nome Fragment ao comentario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import NotificationButton from "./components/NotificationButton"
 function App() {
   return(
-    <> { /*  dentro de um component react so podemos ter uma teg, entao para adicionarmos mais uma podemos colocar esse <> </> para inserirmos outras  */}
+    <> { /* Fragmente pois, dentro de um component react so podemos ter uma teg, entao para adicionarmos mais uma podemos colocar esse <> </> para inserirmos outras  */}
     <h1>Hello world</h1>
     <NotificationButton />
     </>
