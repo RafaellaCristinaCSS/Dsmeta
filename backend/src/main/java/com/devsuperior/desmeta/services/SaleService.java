679e9a165e8fe2f86a63f42faaecde0633ab17b7
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-16 16:03:15 -0300

    importacao necessaria para a execucao da class service

--- backend/src/main/java/com/devsuperior/desmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/desmeta/services/SaleService.java
@@ -1,11 +1,11 @@
 package com.devsuperior.desmeta.services;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.desmeta.entities.Sale;
+import com.devsuperior.desmeta.repositories.SaleRopository; 
 @Service
 public class SaleService {
 	@Autowired 
