344b1921e9b3893dc2a4f36e99bcddbb317b4ccc
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-16 20:03:39 -0300

    adicionando informacoes necessarias para enviar as notificacoes por telefone

--- backend/src/main/java/com/devsuperior/desmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/desmeta/services/SmsService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.desmeta.services;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.desmeta.entities.Sale;
+import com.devsuperior.desmeta.repositories.SaleRopository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -22,14 +26,21 @@ public class SmsService {
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
 
-	public void sendSms() {
+	@Autowired
+	private SaleRopository saleRopository;
+	
+	public void sendSms(Long saleId) {
 
+		Sale sale = saleRopository.findById(saleId).get();
+		
+		String msg = "vendedor " + sale.getSeller_name();
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "Teste").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}
