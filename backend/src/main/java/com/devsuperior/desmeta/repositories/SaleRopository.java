d61cf573cdf8461e4e2d370391ab7c5938bbe27a
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-16 16:02:36 -0300

    criando uma class para repositorio

--- backend/src/main/java/com/devsuperior/desmeta/repositories/SaleRopository.java
+++ backend/src/main/java/com/devsuperior/desmeta/repositories/SaleRopository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.desmeta.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.devsuperior.desmeta.entities.Sale;
+public interface SaleRopository extends JpaRepository<Sale, Long> {
 
-public class SaleRopository extends JpaRepository<Sale, Long> {
-
-}
+} 
