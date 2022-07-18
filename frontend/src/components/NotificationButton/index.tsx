c165d89e1099df11edfa018a764d85e0e0fbdc1c
Rafaella Cristina De Sousa Sacramento <rafacssacrament@gmail.com>
2022-07-18 20:09:57 -0300

    enviando notificacao atraves do botao

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,23 @@
-import icon_perfil from '../../Assets/img/notification_icon.svg'
-import './style.css'
-function NotificationButton(){ 
+import axios from 'axios';
+import icon_perfil from '../../Assets/img/notification_icon.svg';
+import { BASE_URL } from '../../utils/request';
+import './style.css';
+type Props = {
+    saleId: number;
+}
+function handleClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(
+            response => {
+                console.log("Sucesso")
+            }
+        );
+}
+function NotificationButton( {saleId} : Props){ 
     return (
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={()=>{
+                handleClick(saleId);
+            }}>
                 <img src={icon_perfil} alt="Notificar" />
             </div>
      )
